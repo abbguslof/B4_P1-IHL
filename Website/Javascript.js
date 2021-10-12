d9840227a3eb70bf801a6442a69053b56cc87740
ABBerloblo <erling.blomberg@abbgymnasiet.se>
2021-10-12 09:09:25 +0200

    lol

--- Website/Javascript.js
+++ Website/Javascript.js
@@ -2,7 +2,8 @@ x = prompt("Luca the little man!")
 if (x == "yes") {
     alert("U right")
 } if (x == "din mamma") {
-    alert("Erling vill ta ut tjejer t skogen, oh shit here we go again!!!")
+    alert("Luca har en konstig maskara!")
+    window.close
 } else {
     alert("Nah bruh den e fett liten")
     window.close()
