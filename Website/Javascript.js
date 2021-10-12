ae668aa15317feed20cb95461dd2addd03923c8d
abb21lucbih <91330884+LucaBih@users.noreply.github.com>
2021-10-12 08:45:03 +0200

    mod

--- Website/Javascript.js
+++ Website/Javascript.js
@@ -2,7 +2,9 @@
 x = prompt("Har Gustav liten snopp???")
 if(x == "yes") {
     alert("U right")
+} if(x == "din mamma"){
+    alert("Erling vill ta ut tjejer t skogen, oh shit here we go again!!!")
 } else {
     alert("Nah bruh den e fett liten")
     window.close()
-}
+} 
