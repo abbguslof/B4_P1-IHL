8905276f15bddacbb5f02fe6a74a44d8809b5f0c
abbchrjer <92145483+abbchrjer@users.noreply.github.com>
2021-10-12 14:45:20 +0200

    + date

--- Website/Javascript.js
+++ Website/Javascript.js
@@ -4,6 +4,6 @@ if (x == "yes") {
 } if (x == "din mamma") {
     alert("Erling vill ta ut tjejer t skogen, oh shit here we go again!!!")
 } else {
-    alert("Nah bruh den e fett liten")
+    alert("hej")
     window.close()
 }
