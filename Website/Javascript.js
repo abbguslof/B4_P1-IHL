9cb6aac4547cb0636a68c9b4fcd2c41cd1933dc1
abb21lucbih <91330884+LucaBih@users.noreply.github.com>
2021-10-19 12:01:01 +0200

    ++

---
+++ Website/Javascript.js
@@ -0,0 +1,27 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.2/firebase-app.js"
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.1.2/firebase-analytics.js"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBhy84ujYdgfTDt4TwvFpHd_aJy_X3B8R0",
+    authDomain: "abb-temp-project-d1548.firebaseapp.com",
+    databaseURL: "https://abb-temp-project-d1548-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "abb-temp-project-d1548",
+    storageBucket: "abb-temp-project-d1548.appspot.com",
+    messagingSenderId: "1054092009303",
+    appId: "1:1054092009303:web:dc9e55016d78a26066e812",
+    measurementId: "G-JC26DGQY7X"
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+const analytics = getAnalytics(app)
+
+async function gettemp (db) {
+    const
+
+}
