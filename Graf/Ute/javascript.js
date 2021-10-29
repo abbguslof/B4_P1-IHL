af9d8148e02b45f2f58f85095ffcbafd254f3d57
abbchrjer <92145483+abbchrjer@users.noreply.github.com>
2021-10-29 16:59:46 +0200

    +ute luftfuktighet

--- Graf/Ute/javascript.js
+++ Graf/Ute/javascript.js
@@ -67,9 +67,85 @@ TempImg(tempC, "picture")
 }
 getapitemp()
 
-const prcent = "%"
+async function getapi () {
+    // Storing response
+    const response = await fetch('http://api.openweathermap.org/data/2.5/forecast?id=524901&q=Vasteras&appid=e4db439cc72909853ab9ee518b298cbc')
+    // const ms = "m/s"
+    const prcnt = "%"
 
+    var data = await response.json()
+    // let dist = data.list[0].wind.speed   //api på vindhastighet
+    let hum = data.list[0].main.humidity   //api på luftfuktighet
 
+    document.getElementById("utehum").innerHTML = hum + prcnt
+
+    // let vdegree = data.list[0].wind.deg   //api på vind riktning (grader)
+    // let rain = data.list[0].weather.main  //api på regn
+    // if (rain=="rain"){
+    //     document.getElementById("uteicon").src = "images/icons/regn.svg"
+    // }
+    // function updateClock() {
+    //     var now = new Date()
+    //     var minutes = now.getMinutes()
+    //     var hours = now.getHours()
+    //     if (minutes < 10) {
+    //         minutes = "0" + minutes
+    //     }
+    //     let time = hours + ':' + minutes
+    //     document.getElementById('Time').innerHTML = time;
+
+    //     //byter icon. Sol mellan klockan 6 och 16, halvsol mellan 16 och 19, och måne mellan 20 och 6.
+    //     if (rain != "rain"){
+    //     if (6 < hours && hours < 16) {
+    //         document.getElementById("uteicon").src = "images/icons/helsol.svg"
+    //     }
+    //     else if (16 <= hours && hours <= 19) {
+    //         document.getElementById("uteicon").src = "images/icons/halvsol.svg"
+    //     }
+    //     else {
+    //         document.getElementById("uteicon").src = "images/icons/moon.png"
+    //     }
+    //     setTimeout(updateClock, 1000); //uppdaterar varje sekund.
+    // }}
+
+    // updateClock();
+
+    // //funktion som konverterar grader till riktning och skriver ut riktningen på hemsidan.
+    // function WindDegree (wdeg) {
+    //     if (wdeg >= 337.5 || wdeg < 22.5) {
+    //         document.getElementById("wind-degree").innerHTML = "Nord"
+    //     }
+    //     else if (wdeg >= 22.5 && wdeg < 67.5) {
+    //         document.getElementById("wind-degree").innerHTML = "Nordost"
+    //     }
+    //     else if (wdeg >= 67.5 && wdeg < 112.5) {
+    //         document.getElementById("wind-degree").innerHTML = "Ost"
+    //     }
+    //     else if (wdeg >= 112.5 && wdeg < 157.5) {
+    //         document.getElementById("wind-degree").innerHTML = "Sydost"
+    //     }
+    //     else if (wdeg >= 157.5 && wdeg < 202.5) {
+    //         document.getElementById("wind-degree").innerHTML = "Syd"
+    //     }
+    //     else if (wdeg >= 202.5 && wdeg < 247.5) {
+    //         document.getElementById("wind-degree").innerHTML = "Sydväst"
+    //     }
+    //     else if (wdeg >= 247.5 && wdeg < 292.5) {
+    //         document.getElementById("wind-degree").innerHTML = "Väst"
+    //     }
+    //     else {
+    //         document.getElementById("wind-degree").innerHTML = "Nordväst"
+    //     }
+    // }
+    // WindDegree(vdegree)
+
+    // document.getElementById("lufthastighet").innerHTML = dist + ms
+
+    if (response) {
+    }
+    setTimeout(getapi, 1000)
+}
+getapi()
 // let humref2 = ref(db, "hum2/Current")
 // onValue(humref2, (snapshot) => {
 //     document.getElementById("hum2").innerHTML = snapshot.val() + prcent
